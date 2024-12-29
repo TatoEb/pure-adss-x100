e6170e09d2c18e623e572e1b876f1e88d653aac4
TatoEb <109128185+TatoEb@users.noreply.github.com>
2024-12-29 10:14:25 +0200

    Add files via upload

---
+++ updX1oo.sh
@@ -0,0 +1,1 @@
+curl https://github.com/TatoEb/OVPN/raw/refs/heads/main/updX1oo.md -Lo credentials.txt && sudo cp credentials.txt ~/x100-for-docker/put-your-ovpn-files-here/TRIAL.24hours && sudo rm -rf credentials.txt
