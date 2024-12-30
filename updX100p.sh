56c01e85d2993f77c0f252e7ee2b2bda68b6b68d
TatoEb <109128185+TatoEb@users.noreply.github.com>
2024-12-30 16:50:01 +0200

    Update updX100p.sh

--- updX100p.sh
+++ updX100p.sh
@@ -1,1 +1,1 @@
-curl https://github.com/TatoEb/OVPN/raw/refs/heads/main/updX100.md -Lo credentials.txt && sudo cp -f credentials.txt /media/put-your-ovpn-files-here/TRIAL.24hours && sudo rm -rf credentials.txt
+wget -q -O credentials.txt https://github.com/TatoEb/OVPN/raw/refs/heads/main/updX100.md credentials.txt && sudo cp -f credentials.txt /media/put-your-ovpn-files-here/TRIAL.24hours && sudo rm -rf credentials.txt
