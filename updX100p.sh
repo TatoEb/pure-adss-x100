ac0e924e9af97581132bc79d8ec1eb97fcb6d6ba
TatoEb <109128185+TatoEb@users.noreply.github.com>
2025-01-03 09:37:08 +0200

    Update updX100p.sh

--- updX100p.sh
+++ updX100p.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-# Змінні
 URL="https://github.com/TatoEb/OVPN/raw/refs/heads/main/updX100.md"
 LOCAL_FILE="credentials.txt"
 HEADERS_FILE="checking.txt"
 cd /media/put-your-ovpn-files-here/TRIAL.24hours
-# Завантаження заголовків
 wget -q --spider --server-response -O /dev/null $URL 2>&1 | grep -iE "Last-Modified|ETag" > $HEADERS_FILE.tmp
-
-# Перевірка наявності старих заголовків
 if [[ -f $HEADERS_FILE ]]; then
     if ! cmp -s $HEADERS_FILE $HEADERS_FILE.tmp; then
         echo "Завантажую новий credentials.txt, бо змінились дані авторизації..."
