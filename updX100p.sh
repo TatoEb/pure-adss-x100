44dd4d8b977cfa9ef8c567e31c6316b86ce8a9e0
TatoEb <109128185+TatoEb@users.noreply.github.com>
2025-02-05 13:39:57 +0200

    Update updX100p.sh

--- updX100p.sh
+++ updX100p.sh
@@ -9,7 +9,7 @@ if [[ -f $HEADERS_FILE ]]; then
         echo "A newer file 'credentials.txt' is downloading..."
         wget -q -O $LOCAL_FILE $URL
         mv $HEADERS_FILE.tmp $HEADERS_FILE
-        echo "Your new credentils are bellow:"
+        echo "New credentials are bellow:"
         cat /media/put-your-ovpn-files-here/TRIAL.24hours/credentials.txt
     else
         echo "There is no change in the file 'credentials.txt'."
