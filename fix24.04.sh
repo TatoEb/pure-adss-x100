abef6ffb9285529954a01b977f90470ada38a6ad
TatoEb <109128185+TatoEb@users.noreply.github.com>
2025-02-05 13:26:43 +0200

    Update fix24.04.sh

--- fix24.04.sh
+++ fix24.04.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if grep -q "Ubuntu 24.04" /etc/os-release; then
-    cd /etc/apt/sources.list.d && sudo sed -i 's/noble/jammy/g' ookla_speedtest-cli.list && cd ~
+    echo "Ubuntu 24.04 is detected, a fixing script has been applied" && cd /etc/apt/sources.list.d && sudo sed -i 's/noble/jammy/g' ookla_speedtest-cli.list && cd ~
 fi
