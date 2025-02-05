83213966596036aea5f5372951ef7e8bb1e86059
TatoEb <109128185+TatoEb@users.noreply.github.com>
2025-02-05 09:09:00 +0200

    Update updX100p.sh

--- updX100p.sh
+++ updX100p.sh
@@ -6,15 +6,15 @@ cd /media/put-your-ovpn-files-here/TRIAL.24hours
 wget -q --spider --server-response -O /dev/null $URL 2>&1 | grep -iE "Last-Modified|ETag" > $HEADERS_FILE.tmp
 if [[ -f $HEADERS_FILE ]]; then
     if ! cmp -s $HEADERS_FILE $HEADERS_FILE.tmp; then
-        echo "Завантажую новий credentials.txt, бо змінились дані авторизації..."
+        echo "A newer file 'credentials.txt' is downloading..."
         wget -q -O $LOCAL_FILE $URL
         mv $HEADERS_FILE.tmp $HEADERS_FILE
     else
-        echo "Дані авторизації в файлі credentials.txt не змінились."
+        echo "There is no change in the file 'credentials.txt'."
         rm $HEADERS_FILE.tmp
     fi
 else
-    echo "Завантажую файл credentials.txt..."
+    echo "The file 'credentials.txt' is downloading..."
     wget -q -O $LOCAL_FILE $URL
     mv $HEADERS_FILE.tmp $HEADERS_FILE
 fi
