a89aaa5378c0cc207d5b5bd92c8ec59aadb44240
TatoEb <109128185+TatoEb@users.noreply.github.com>
2025-02-25 20:32:47 +0200

    Update X100p.sh

--- X100p.sh
+++ X100p.sh
@@ -1,1 +1,1 @@
-cd ~ && sudo chmod 777 -R /media/put-your-ovpn-files-here && sudo screen -S X100 bash /root/x100/x100-run.bash && cd ~
+cd ~ && sudo chmod 777 -R /media/put-your-ovpn-files-here && cd /media/put-your-ovpn-files-here && sudo rm -rf *log*.txt && sudo screen -S X100 bash /root/x100/x100-run.bash && cd ~
